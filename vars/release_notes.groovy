a936e21f91759a6bfa50445ce6bc3ddeb72a1112
fimtestuvw <82125914+fimtestuvw@users.noreply.github.com>
2021-05-14 08:18:22 +0800

    edit vars/release_notes.groovy

--- vars/release_notes.groovy
+++ vars/release_notes.groovy
@@ -1,30 +1,46 @@
 import groovy.io.FileType;
 import java.io.File;
+import java.util.Calendar.*;
 import java.text.SimpleDateFormat
+import hudson.model.*
 
 @NonCPS
-def call(Map config=[:]){
+def call(Map config=[:])
+{
 	def dir = new File(pwd());
 
 	new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
 	{
 		writer ->
-	    dir.eachFileRecurse(FileType.ANY){ file ->
-		if (file.isDirectory()){
-		    writer.writeLine(file.name);
+
+		def now = new Date();
+		def fmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		writer.writeLine("Date and Time IS: " + fmt.format(now));
+
+		writer.writeLine("Build Number is: ${BUILD_NUMBER}");
+
+		if (config.changes != "false"){
+			def changeLogSets = currentBuild.changeSets;
+
+			for (change in changeLogSets) {
+				def entries = change.items;
+				for (entry in entries) {
+					writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}");
+					for (file in entry.affectedFiles) {
+						writer.writeLine("${file.editType.name} ${file.path}");
+					}
+				}
+			}
 		}
-		else
-		{
-		    writer.writeLine('\t' + file.name + '\t' + file.length());
-		}
+
+		dir.eachFileRecurse(FileType.ANY){ file ->
+			if (file.isDirectory()){
+				writer.writeLine(file.name);
+			}
+			else
+			{
+				writer.writeLine('\t' + file.name + '\t' + file.length());
+			}
 		}
 	}
-
-	def date = new Date()
-	def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-	echo "Date and Time IS: " + sdf.format(date)
-
-    if (config.changes != "false"){
-    	echo "changes";
-    }
 }
